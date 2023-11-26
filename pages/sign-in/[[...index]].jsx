a21917c5c26b893a661e69d49d7d005b6131a41d
Your Name <abirdutta90990@gmail.com>
2023-11-26 18:15:33 +0530

    Addiong Headers

--- pages/sign-in/[[...index]].jsx
+++ pages/sign-in/[[...index]].jsx
@@ -1,8 +1,11 @@
 import { SignIn } from "@clerk/nextjs";
-
+import Head from "next/head";
 export default function Page() {
   return (
     <div className="bg-[url('/signInBg.jpeg')] flex justify-center items-center h-screen">
+       <Head>
+        <title>Sign In</title>
+      </Head>
       <SignIn />
     </div>
   )
--- pages/sign-up/[[...index]].jsx
+++ pages/sign-up/[[...index]].jsx
@@ -1,8 +1,11 @@
 import { SignUp } from "@clerk/nextjs";
- 
+ import Head from "next/head";
 export default function Page() {
   return (
     <div className=" bg-[url('/signInBg.jpeg')] flex justify-center items-center h-screen">
+       <Head>
+        <title>Sign Up</title>
+      </Head>
     <SignUp />
   </div>
   )
